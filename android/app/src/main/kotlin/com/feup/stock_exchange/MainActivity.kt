af4590788673d9138af9e0bfa4892076dedd02a9
diogobabo <diogo.babo@gmail.com>
2024-05-20 19:47:39 +0100

    second

---
+++ android/app/src/main/kotlin/com/feup/stock_exchange/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.feup.stock_exchange
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/homeScreenWidget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../controllers/homeScreenState.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({
+    super.key,
+  });
+
+  @override
+  State<HomeScreen> createState() => HomeScreenState();
+}
